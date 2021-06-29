7266feec12ad91393d352fbe2b17808df7aa098d
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-28 23:15:00 -0500

    Imprimiendo en DOM la informacion de la base de datos simulada

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Variables
+const resultado = document.querySelector('#resultado');
+
+// Eventos
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+});
+
+
+
+// Funciones
+function mostrarAutos() {
+    autos.forEach(auto => {
+        const autoHTML = document.createElement('p');
+
+        autoHTML.textContent = `
+            ${auto.marca}
+        `;
+
+        // Insertar en el html
+        resultado.appendChild(autoHTML);
+
+    })
+}
+
+
