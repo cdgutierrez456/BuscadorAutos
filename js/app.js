a4997745ec225b0f43cd516487755ebf215e1a30
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-29 14:56:30 -0500

    Creando objeto con terminos de filtrado. Capturando cambios en desplegables y asignando valores a objeto creado

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor de los resultados
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+// Generando un objeto con los datos de busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 
 // Eventos
@@ -12,6 +31,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Llenando las opciones de años
     llenarSelect();
+});
+
+// Capturando eventos de los select
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
 });
 
 
